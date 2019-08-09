0125ae8bc6eecd5b5786923b3808495ab4d14b80
hdhhdh <dhgks4002@naver.com>
2019-08-09 16:55:43 +0900

    BOJ_14501 commit

---
+++ BOJ_14501.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int ti[15];
+int pi[15];
+int dp[15];
+
+int main()
+{
+	cin >> n;
+	for(int i = 0; i<n; i++)
+	{
+		cin >> ti[i] >> pi[i];
+	}
+	
+	for(int i = n-1; i>=0; i--)
+	{
+		if(i + ti[i] >= n + 1)
+		{
+			dp[i] = 0;
+			continue;
+		} 
+		dp[i] = max(dp[i+1], dp[i + ti[i]] + pi[i]);
+	}
+	
+	cout << dp[0];
+
+	return 0;
+	
+}
